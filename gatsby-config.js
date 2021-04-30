da22b140642867e42aad7a03ee41c50e17b75cc0
Piotr Gibek <piotrgibek@outlook.com>
2021-05-01 01:40:51 +0200

    add github action

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Starlink Map',
   },
-  plugins: ['gatsby-plugin-styled-components', `gatsby-plugin-pnpm`],
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-pnpm',
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: 'starlink-map-website',
+      },
+    },
+  ],
 };
