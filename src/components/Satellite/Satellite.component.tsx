5985f0ff1d6c3afd9db82971d6a0e0629ca61692
Piotr Gibek <piotrgibek@outlook.com>
2021-09-21 23:18:08 +0200

    add experimental click handler

--- src/components/Satellite/Satellite.component.tsx
+++ src/components/Satellite/Satellite.component.tsx
@@ -1,18 +1,37 @@
 import * as React from 'react';
 import { EciVec3 } from 'satellite.js';
+import { useEffect, useState } from 'react';
 
 export interface SatelliteProps {
   positionVector: EciVec3<number>;
 }
 
 const Satellite: React.FC<SatelliteProps> = ({ positionVector }) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  useEffect(() => {
+    document.body.style.cursor = hovered ? 'pointer' : 'auto';
+  }, [hovered]);
+
   if (!positionVector) return null;
 
   return (
-    <mesh position={[positionVector.y, positionVector.z, positionVector.x]}>
-      <sphereBufferGeometry args={[15, 15, 15]} />
-      <meshBasicMaterial color="white" />
-    </mesh>
+    <>
+      <mesh
+        position={[positionVector.y, positionVector.z, positionVector.x]}
+        onClick={() => setIsClicked(true)}
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+      >
+        <sphereBufferGeometry args={[90, 7, 7]} />
+        <meshBasicMaterial opacity={0} transparent />
+      </mesh>
+      <mesh position={[positionVector.y, positionVector.z, positionVector.x]}>
+        <sphereBufferGeometry args={[15, 15, 15]} />
+        <meshBasicMaterial color={isClicked ? 'red' : 'white'} />
+      </mesh>
+    </>
   );
 };
 
